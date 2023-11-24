101b5d57a9a79584f0b1a9634a731bf98386e5a7
LiviaNobre <livianobrelp@gmail.com>
2023-11-23 21:23:18 -0300

    :sparkles: feat Day 1 - Exercícios e projeto

---
+++ Day_1/Project_1.py
@@ -0,0 +1,9 @@
+# %%
+
+print("What's name of the city you grow up?")
+city = input()
+print(city)
+print("What's your pet's name?")
+pet = input()
+print(pet)
+print(f"Your band name could be {city} {pet}")
